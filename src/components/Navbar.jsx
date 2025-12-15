6562e937553c9d16f3f1f8bba5f10f11511bc839
zeeshanwdev27 <zeeshanwdev27@gmail.com>
2025-12-16 02:31:53 +0500

    do responsive these pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ function Navbar() {
 
   const [showMenu, setShowMenu] = useState(false)
   const [token, setToken] = useState(true)
+  const [menuOpen, setMenuOpen] = useState(false);
 
 
   return (
@@ -33,10 +34,10 @@ function Navbar() {
       </ul>
       <div className="flex items-center gap-4">
         {
-          token ? <div className="flex items-center gap-2 cursor-pointer group relative">
+          token ? <div className="flex items-center gap-2 cursor-pointer group relative" onClick={() => setMenuOpen(!menuOpen)}>
             <img className="w-8 rounded-full" src={assets.profile_pic} alt="profile_pic" />
             <img className="w-2.5" src={assets.dropdown_icon} alt="dropdown_icon" />
-            <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
+            <div className={`absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 group-hover:block  ${ menuOpen ? "block" : "hidden"}`}>
               <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
                 <p onClick={()=> navigate('/my-profile')} className="hover:text-black cursor-pointer">My Profile</p>
                 <p onClick={()=> navigate('/my-appointments')} className="hover:text-black cursor-pointer">My Appointments</p>
@@ -44,8 +45,29 @@ function Navbar() {
               </div>
             </div>
           </div>
-          : <button onClick={()=> navigate('/login')} className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block cursor-pointer">Create Account</button>
+          : <button onClick={()=> navigate('/login')} className="bg-primary text-white px-8 py-3 rounded-full font-light  cursor-pointer">Create Account</button>
         }
+
+        {/* HamBurger Img */}
+        <img onClick={()=> setShowMenu(true)} className="w-6 md:hidden" src={assets.menu_icon} alt="" />
+
+        {/* -------Mobile Menu-----------  */}
+        <div className={`${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+
+          <div className="flex items-center justify-between px-5 py-6">
+            <img className="w-36" src={assets.logo} alt="" />
+            <img className="w-7" onClick={()=> setShowMenu(false)} src={assets.cross_icon} alt="" />
+          </div>
+
+          <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+            <NavLink  onClick={()=>setShowMenu(false)} to={'/'}><p className='px-4 py-2 rounded inline-block'>HOME</p></NavLink>
+            <NavLink  onClick={()=>setShowMenu(false)} to={'/doctors'}><p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p></NavLink>
+            <NavLink  onClick={()=>setShowMenu(false)} to={'/about'}><p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
+            <NavLink  onClick={()=>setShowMenu(false)} to={'/contact'}><p className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
+          </ul>
+
+        </div>
+
       </div>
     </div>
   );
--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -8,6 +8,9 @@ function Doctors() {
   const { speciality } = useParams()
   const { doctors }  = useContext(AppContext)     //bcz i store assets.js data in context
   const [ filterDoc, setFilterDoc ] = useState([])
+  const [showFilter, setShowFilter] = useState(false)
+
+
   const navigate = useNavigate()
 
 
@@ -33,8 +36,10 @@ function Doctors() {
       <p className='text-gray-600'>Browse through the doctors speciality.</p>
 
       <div className='flex flex-col sm:flex-row items-start gap-10 mt-5'>
+
+        <button onClick={()=> setShowFilter(prev => !prev)} className={`py-1 px-3 border border-primary text-primary font-semibold rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary text-white': ''}`}>Show Filters</button>
  
-        <div className='flex flex-col gap-4 text-sm text-gray-600'>
+        <div className={`flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex': 'hidden sm:flex'}`}>
           <p onClick={()=> speciality === 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'General physician' ? 'bg-indigo-100 text-black' : ''}`}>General physician</p>
           <p onClick={()=> speciality === 'Gynecologist' ? navigate('/doctors') : navigate('/doctors/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Gynecologist' ? 'bg-indigo-100 text-black' : ''}`}>Gynecologist</p>
           <p onClick={()=> speciality === 'Dermatologist' ? navigate('/doctors') : navigate('/doctors/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Dermatologist' ? 'bg-indigo-100 text-black' : ''}`}>Dermatologist</p>
