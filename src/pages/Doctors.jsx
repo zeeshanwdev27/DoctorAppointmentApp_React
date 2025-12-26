950b33b2ebe32d1f57168a06584412d67edeb806
zeeshanwdev27 <zeeshanwdev27@gmail.com>
2025-12-27 04:00:23 +0500

    fix available badge

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -54,8 +54,8 @@ function Doctors() {
             <div onClick={()=> navigate(`/appointment/${item._id}`)} key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:-translate-y-2.5 transition-all duration-500'>
               <img src={item.image} alt={item.name} className='bg-blue-50' />
               <div className='p-4'>
-                <div className='flex items-center gap-2 text-sm text-center text-green-500'>
-                  <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                <div className={`flex items-center gap-2 text-sm text-center ${ item.available ? 'text-green-500' : 'text-red-500'}`}>
+                  <p className={`w-2 h-2 ${ item.available ? 'bg-green-500' : 'bg-red-500'} rounded-full`}></p><p>{ item.available ? 'Available' : 'Not Available' }</p>
                 </div>
                 <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
                 <p className='text-gray-600 text-sm'>{item.speciality}</p>
